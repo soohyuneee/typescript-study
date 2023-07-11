ca3988d3dc2c21370b1dc9f05d9741ca4ace1d8a
soohyuneee <tlstngus95@naver.com>
2023-07-11 18:33:23 +0900

    230711

---
+++ section8/src/chapter0.ts
@@ -0,0 +1,43 @@
+/**
+ * 인덱스드 엑세스 타입
+ */
+
+import { type } from 'os';
+
+type PostList = {
+  title: string;
+  content: string;
+  author: {
+    id: number;
+    name: string;
+    age: number;
+  };
+}[];
+
+function printAuthorInfo(author: PostList[number]['author']) {
+  console.log(`${author.name}-${author.id}`);
+}
+
+const post: PostList[0] = {
+  title: '게시글 제목',
+  content: '본문',
+  author: {
+    id: 1,
+    name: 'soo',
+    age: 27,
+  },
+};
+
+printAuthorInfo(post.author);
+
+type Tup = [number, string, boolean];
+
+type Tup0 = Tup[0];
+
+type Tup1 = Tup[1];
+
+type Tup2 = Tup[2];
+
+// type Tup3 = Tup[3];
+
+type TupNum = Tup[number];
